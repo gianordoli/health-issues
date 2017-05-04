77150aa36ff75831af1b689155e30da80b88d433
gianordoli <gianordoligabriel@yahoo.com.br>
2017-05-03 21:40:13 -0400

    Fixed date parsing

--- src/js/LineChart.js
+++ src/js/LineChart.js
@@ -8,7 +8,10 @@ import * as d3 from 'd3';
 
 export class LineChart {
 
-  data: {date: Date, value: number}[];
+  data: {
+    term: string,
+    points: {date: Date, value: number}[]
+  }[];
   type: string;
   svg: () => {};
   x: () => {};
@@ -29,15 +32,19 @@ export class LineChart {
 
   updateData(data: TrendsAPIData[]) {
     this.data = this.parseDates(data);
+    console.log('D3 ->', this.data);
     this.updateElements();
   }
 
   parseDates(data: TrendsAPIData[]) {
     const parseTime = d3.timeParse('%Y-%m-%d');
     return data.map((d, i) => {
-      d.points.map((p, i) => {
-        return { date: parseTime(p), value: p.value }
-      })
+      return {
+        term: d.term,
+        points: d.points.map((p, i) => {
+          return { date: parseTime(p.date), value: p.value }
+        })
+      }
     });
   }
 
@@ -66,6 +73,7 @@ export class LineChart {
     this.yAxis = d3.axisLeft(y);
 
     this.line = d3.line()
+      .curve(d3.curveBasis)
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.value); });
 
@@ -78,8 +86,15 @@ export class LineChart {
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    this.x.domain(d3.extent(data, function(d) { return d.date; }));
-    this.y.domain(d3.extent(data, function(d) { return d.value; }));
+    // this.x.domain(d3.extent(data[0].points, function(p) { return p.date; }));
+    // this.x.domain(d3.extent(data[0].points, function(p) { return p.date; }));
+    this.x.domain( d3.extent(data, function(d, i) {
+        return d3.extent(d.points, function(p) { return p.date }) })
+    );
+    this.y.domain([
+      d3.min(data, function(d, i) { return d3.min(d.points, function(p) { return p.value; }); }),
+      d3.max(data, function(d, i) { return d3.max(d.points, function(p) { return p.value; }); })
+    ]);
 
     chart.append('g')
       .attr('class', 'x axis')
@@ -88,21 +103,37 @@ export class LineChart {
 
     chart.append('g')
       .attr('class', 'y axis')
-      .call(this.yAxis)
+      .call(this.yAxis);
 
-    chart.append('path')
-      .datum(data)
-      .attr('class', 'line')
+    const disease = chart.selectAll(".disease")
+      .data(data)
+      .enter()
+      .append("g")
+      .attr("class", "disease");
+
+    disease.append("path")
+      .attr("class", "line")
       .attr('stroke', 'black')
-      .attr('d', this.line);
+      .attr("d", function(d) { return this.line(d.points) });
+
+    // chart.append('path')
+    //   .datum(data)
+    //   .attr('class', 'line')
+    //   .attr('stroke', 'black')
+    //   .attr('d', this.line);
   }
 
   updateElements() {
     const { data } = this;
     let { svg, x, y, xAxis, yAxis, line  } = this;
 
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.value; }));
+    this.x.domain( d3.extent(data, function(d, i) {
+        return d3.extent(d.points, function(p) { return p.date }) })
+    );
+    this.y.domain([
+      d3.min(data, function(d, i) { return d3.min(d.points, function(p) { return p.value; }); }),
+      d3.max(data, function(d, i) { return d3.max(d.points, function(p) { return p.value; }); })
+    ]);
 
     svg.select('g.y')
       .transition()
@@ -113,6 +144,17 @@ export class LineChart {
       .transition()
       .duration(1000)
       .call(xAxis);
+
+    // const disease = d3.selectAll(".disease")
+    //   .data(data)
+    //   .enter()
+    //   .append("g")
+    //   .attr("class", "disease");
+    //
+    // disease.append("path")
+    //   .attr("class", "line")
+    //   .attr('stroke', 'black')
+    //   .attr("d", function(d) { return this.line(d.points) });
 
     svg.selectAll('path.line')
       .datum(data)
