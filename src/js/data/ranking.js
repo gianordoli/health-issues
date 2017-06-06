29347c1925d248656eea1a4df247392ff54de473
gianordoli <gianordoligabriel@yahoo.com.br>
2017-06-06 13:57:45 -0400

    Styling ranking

--- src/js/data/ranking.js
+++ src/js/data/ranking.js
@@ -1,54 +1,54 @@
 module.exports = [
     {
       year: 2004,
-      diseases: ["Pain","Cancer","HIV/AIDS","Diabetes mellitus","Depression","Allergy","Amyotrophic lateral sclerosis","Stress","Infection","Obesity"],
+      diseases: ["Pain","Cancer","HIV/AIDS","Diabetes","Depression","Allergy","ALS","Stress","Infection","Obesity"],
     },
     {
       year: 2005,
-      diseases: ["Pain","Cancer","Diabetes mellitus","HIV/AIDS","Depression","Infection","Amyotrophic lateral sclerosis","Allergy","Stress","Burn"],
+      diseases: ["Pain","Cancer","Diabetes","HIV/AIDS","Depression","Infection","ALS","Allergy","Stress","Burn"],
     },
     {
       year: 2006,
-      diseases: ["Pain","Cancer","Diabetes mellitus","HIV/AIDS","Depression","Allergy","Infection","Stress","Amyotrophic lateral sclerosis","Acne"],
+      diseases: ["Pain","Cancer","Diabetes","HIV/AIDS","Depression","Allergy","Infection","Stress","ALS","Acne"],
     },
     {
       year: 2007,
-      diseases: ["Pain","Cancer","Diabetes mellitus","HIV/AIDS","Depression","Allergy","Amyotrophic lateral sclerosis","Infection","Stress","Acne"],
+      diseases: ["Pain","Cancer","Diabetes","HIV/AIDS","Depression","Allergy","ALS","Infection","Stress","Acne"],
     },
     {
       year: 2008,
-      diseases: ["Pain","Cancer","Diabetes mellitus","HIV/AIDS","Allergy","Infection","Amyotrophic lateral sclerosis","Depression","Acne","Stress"],
+      diseases: ["Pain","Cancer","Diabetes","HIV/AIDS","Allergy","Infection","ALS","Depression","Acne","Stress"],
     },
     {
       year: 2009,
-      diseases: ["Pain","Cancer","Diabetes mellitus","Influenza","HIV/AIDS","Acne","Allergy","Infection","Amyotrophic lateral sclerosis","Depression"],
+      diseases: ["Pain","Cancer","Diabetes","Influenza","HIV/AIDS","Acne","Allergy","Infection","ALS","Depression"],
     },
     {
       year: 2010,
-      diseases: ["Pain","Cancer","Diabetes mellitus","Acne","Allergy","Infection","HIV/AIDS","Depression","Amyotrophic lateral sclerosis","Burn"],
+      diseases: ["Pain","Cancer","Diabetes","Acne","Allergy","Infection","HIV/AIDS","Depression","ALS","Burn"],
     },
     {
       year: 2011,
-      diseases: ["Pain","Cancer","Diabetes mellitus","Acne","Allergy","Infection","Depression","Burn","Amyotrophic lateral sclerosis","HIV/AIDS"],
+      diseases: ["Pain","Cancer","Diabetes","Acne","Allergy","Infection","Depression","Burn","ALS","HIV/AIDS"],
     },
     {
       year: 2012,
-      diseases: ["Pain","Cancer","Diabetes mellitus","Acne","Allergy","Headache","Cough","Infection","Itch","Diarrhea"],
+      diseases: ["Pain","Cancer","Diabetes","Acne","Allergy","Headache","Cough","Infection","Itch","Diarrhea"],
     },
     {
       year: 2013,
-      diseases: ["Pain","Cancer","Acne","Diabetes mellitus","Allergy","Headache","Fever","Itch","Diarrhea","Infection"],
+      diseases: ["Pain","Cancer","Acne","Diabetes","Allergy","Headache","Fever","Itch","Diarrhea","Infection"],
     },
     {
       year: 2014,
-      diseases: ["Pain","Cancer","Diabetes mellitus","Acne","Allergy","Amyotrophic lateral sclerosis","Cough","Headache","Itch","Fever"],
+      diseases: ["Pain","Cancer","Diabetes","Acne","Allergy","ALS","Cough","Headache","Itch","Fever"],
     },
     {
       year: 2015,
-      diseases: ["Pain","Cancer","Acne","Diabetes mellitus","Headache","Itch","Allergy","Diarrhea","Cough","Fever"],
+      diseases: ["Pain","Cancer","Acne","Diabetes","Headache","Itch","Allergy","Diarrhea","Cough","Fever"],
     },
     {
       year: 2016,
-      diseases: ["Pain","Cancer","Acne","Diabetes mellitus","Headache","Itch","Allergy","Diarrhea","Cough","Fever"]
+      diseases: ["Pain","Cancer","Acne","Diabetes","Headache","Itch","Allergy","Diarrhea","Cough","Fever"]
     },
 ];
